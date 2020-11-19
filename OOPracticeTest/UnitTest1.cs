ad68261769c0de705b46273c2e6049b6e5d4611a
Ruirui Huang <rhuang2@slb.com>
2020-11-19 10:30:28 +0800

    feat: create two engine classes and realize speed up based on engine type in car/truck

--- OOPracticeTest/UnitTest1.cs
+++ OOPracticeTest/UnitTest1.cs
@@ -6,10 +6,53 @@ namespace OOPracticeTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_Car_Speedup_With_Name_Speed_ElectricEngine()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            // given
+            var car = new Car("Cool Car", new ElectricEngine());
+            // when
+            string expected = "Cool Car: speed up 25 km/h";
+            string actual = car.SpeedUp();
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Truck_Speedup_With_Name_Speed_ElectricEngine()
+        {
+            // given
+            var truck = new Truck("Big Truck", new ElectricEngine());
+            // when
+            string expected = "Big Truck: speed up 25 km/h";
+            string actual = truck.SpeedUp();
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Driver_When_Speedup_Drive_Car_ElectricEngine()
+        {
+            // given
+            var car = new Car("Cool Car", new ElectricEngine());
+            var driver = new Driver(car);
+            // when
+            string expected = "Cool Car: speed up 25 km/h";
+            string actual = driver.SpeedUp();
+            // then
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Should_Driver_When_Speedup_Drive_Truck_GasolineEngine()
+        {
+            // given
+            var car = new Car("Big Truck", new GasolineEngine());
+            var driver = new Driver(car);
+            // when
+            string expected = "Big Truck: speed up 30 km/h";
+            string actual = driver.SpeedUp();
+            // then
+            Assert.Equal(expected, actual);
         }
     }
 }
